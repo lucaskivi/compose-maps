f5c6603946d9a044bf7a316395a733ca262b9100
Lucas Kivi <lucas@livefront.com>
2022-07-23 11:28:58 -0500

    Add Google Maps!

--- app/src/main/java/com/example/composemaps/MainActivity.kt
+++ app/src/main/java/com/example/composemaps/MainActivity.kt
@@ -6,11 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composemaps.ui.theme.ComposeMapsTheme
+import com.google.maps.android.compose.GoogleMap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +17,9 @@ class MainActivity : ComponentActivity() {
             ComposeMapsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    GoogleMap()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMapsTheme {
-        Greeting("Android")
     }
 }
